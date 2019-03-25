434943642bcb4a4ab1bd99a219b9e97f80d2ce80
TongDist666 <2930773981@qq.com>
2019-03-25 14:39:29 +0800

    读取视频，把每一帧图像缩小为原来的一半进行保存

--- write_frame_img.py
+++ write_frame_img.py
@@ -6,16 +6,16 @@ Created on Mon Mar 25 13:46:14 2019
 """
 
 import cv2
-#摄像头编号:0  多摄像头的话记得查看各自的编号
+#打开视频
 cap = cv2.VideoCapture('video.avi')
-#有的书上说X64的电脑上，这个地方要加：#0xFF != 27
-#但是可以不加
+#视频帧从1开始计数
 count=1
 try:
     while cv2.waitKey(1) & 0xFF != 27:
     
     	#frame是一帧的图像
         ret, frame = cap.read()
+        #缩小为原来的一半
         dst=cv2.resize(frame,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_AREA)
         cv2.imwrite('./frame_img/'+str(count)+'.jpg',dst)
         print('writing '+str(count))
